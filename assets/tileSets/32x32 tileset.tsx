ef938920915b6e60b282a0664571a972138802d4
jeremaquai <jeremaquai@gmail.com>
2025-01-25 23:44:05 -0600

    second commit

---
+++ assets/tileSets/32x32 tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.1" name="32x32 first tileset" tilewidth="32" tileheight="32" tilecount="300" columns="30">
+ <image source="../tileSetSpriteSheets/32x32 first tileset.png" trans="000000" width="960" height="320"/>
+</tileset>
